eee8245898fab187efa18da1d1202d39ee5b79f3
fxi <f@fxi.io>
2024-07-06 08:58:13 +0200

    Build in github action failed, re-wrote local build file

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-
-
 echo "Build on push with github actions"
+
+PLATFORMS=linux/amd64,linux/arm64
+IMAGE=fredmoser/inaccessmod:latest
+
+docker build --platform $PLATFORMS -t $IMAGE --push .
